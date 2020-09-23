8243ba010fcde8a192db0b148f87467af5989e87
rifat88 <71731843+rifat88@users.noreply.github.com>
2020-09-23 19:24:01 +0600

    First commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed; 
+    Rigidbody2D rb;
+    
+    
+ 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //flap effect
+            rb.velocity = Vector2.up * speed;
+        }
+    }
+}
