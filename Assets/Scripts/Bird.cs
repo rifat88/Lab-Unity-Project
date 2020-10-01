6f46321cba7884936c633b44bb993e49dfa30aa5
rifat88 <71731843+rifat88@users.noreply.github.com>
2020-10-02 03:34:59 +0600

    Final commit(Score and replay)
    added replay

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
     public float speed; 
     Rigidbody2D rb;
-    
-    
+
+    //references
+    public Score scoreText;
+    public GameObject replayBtn;
  
     void Start()
     {
+        Time.timeScale = 1;
         rb = GetComponent<Rigidbody2D>();
 
     }
@@ -24,4 +28,27 @@ public class Bird : MonoBehaviour
             rb.velocity = Vector2.up * speed;
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Column"))
+        {
+            print("Score Up");
+            scoreText.ScoreUp();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground") ||
+           collision.gameObject.CompareTag("Pipe"))
+        {
+            //game over 
+            Time.timeScale = 0;
+            replayBtn.SetActive(true);
+        }
+    }
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
